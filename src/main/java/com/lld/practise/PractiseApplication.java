ef4de99dc0269caa42b99f532f24af79552687d2
Mihir <montyaswal0@gmail.com>
2025-05-27 01:49:34 +0530

    Async update for shorturl status to USED after short url creation

--- src/main/java/com/lld/practise/PractiseApplication.java
+++ src/main/java/com/lld/practise/PractiseApplication.java
@@ -7,9 +7,11 @@ import com.lld.practise.logger.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @SpringBootApplication
+@EnableAsync
 public class PractiseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lld/practise/systemDesign/urlShortner/repository/AvailableIdRepository.java
+++ src/main/java/com/lld/practise/systemDesign/urlShortner/repository/AvailableIdRepository.java
@@ -1,14 +1,12 @@
 package com.lld.practise.systemDesign.urlShortner.repository;
 
 import com.lld.practise.systemDesign.urlShortner.entity.AvailableId;
-import com.lld.practise.systemDesign.urlShortner.entity.UrlDetails;
 import com.lld.practise.systemDesign.urlShortner.enums.AvailabilityStatus;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +18,6 @@ public interface AvailableIdRepository extends JpaRepository<AvailableId, Long> 
 
     @Query(value = "SELECT * from available_ids WHERE status= :status LIMIT :limit",nativeQuery = true)
     public List<AvailableId> findUnusedAvailablityStatus(@Param("status")AvailabilityStatus status,@Param("limit") int limit);
+
+    public Optional<AvailableId> findByBase64Id(String base64Id);
 }
